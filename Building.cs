e38f4e379c7b6de92a0ea6342d2dd8b6174945ee
Chris Calhoun <ctcalhoun2@gmail.com>
2021-01-21 20:50:50 -0600

    urban planner exercise.

---
+++ Building.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrbanPlanner
+{
+    class Building
+    {
+        // Private Fields
+        private string _designer = "Chris Calhoun";
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
+
+        // Public Properties
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public int Volume 
+        {
+            get
+            {
+                return (int)(Width * Depth * (3 * Stories));
+            }
+        }
+
+        // Constructor
+        public Building(string address)
+        {
+            _address = address;
+        }
+
+        // Methods
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string owner)
+        {
+            _owner = owner;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{_address}---------------");
+            Console.WriteLine($"Designed by {_designer}");
+            Console.WriteLine($"Constructed on {_dateConstructed}");
+            Console.WriteLine($"Owned by {_owner}");
+            Console.WriteLine($"{Volume} cubic meters of space.\n");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace UrbanPlanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Building 2807
+            Building TwoEightZeroSeven = new Building("2807 22nd Ave. S.");
+            TwoEightZeroSeven.Width = 20;
+            TwoEightZeroSeven.Depth = 30;
+            TwoEightZeroSeven.Stories = 5;
+            TwoEightZeroSeven.Construct();
+            TwoEightZeroSeven.Purchase("Barack Obama");
+            TwoEightZeroSeven.PrintInfo();
+
+            // Building 113
+            Building OneOneThree = new Building("1234 Alpha Ct.");
+            OneOneThree.Width = 20;
+            OneOneThree.Depth = 20;
+            OneOneThree.Stories = 2;
+            OneOneThree.Construct();
+            OneOneThree.Purchase("Todd Calhoun");
+            OneOneThree.PrintInfo();
+
+            // Building 5678
+            Building FiveSixSevenEight = new Building("5678 Murphy St.");
+            FiveSixSevenEight.Width = 25;
+            FiveSixSevenEight.Depth = 25;
+            FiveSixSevenEight.Stories = 3;
+            FiveSixSevenEight.Construct();
+            FiveSixSevenEight.Purchase("Derrick Henry");
+            FiveSixSevenEight.PrintInfo();
+        }
+    }
+}
